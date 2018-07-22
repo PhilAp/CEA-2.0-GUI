1f03f1120a414dc8f6814dedde3862311d408c97
PhilAp <Philip_livdan@bentleystudent.net>
2018-07-22 10:07:44 +0300

    Update EditCEA.m

--- EditCEA.m
+++ EditCEA.m
@@ -1,17 +1,21 @@
+%% Preparing Configuration
+fid = fopen(fullfile('D:\','\ConfigCEA.xlsx'),'wt');
+fclose(fid);
+clear fid %creats excel configuration file
+
+filename = 'ConfigCEA.xlsx';
+A = [12.7 5.02 -98 63.9 0 -.2 56];
+xlswrite(filename,A)
+%% Prompt
+prompt = {'Problem Type:','Pressure Units:'};
+title = 'Input';
+dims = [1 35];
+definput = {'20','hsv'};
+answer = inputdlg(prompt,title,dims,definput)
+
 %% Preparing
-s = sprintf('New message in new line\n');
 fid = fopen(fullfile('D:\','test.imp'),'wt');
 fprintf(fid,s)
 fprintf(fid, sprintf('New message in new line'));
 fclose(fid);
 clear fid
-%% manipulation
-txt = regexprep(txt,'^function','\<FUNCTION\>')
-txt = regexprep(txt,'path','\<PATH\>')
-txt = regexprep(txt,'(.*)','$1\n');
-txt = [txt{:}];
-fclose(fid);
-%% writing
-fid = fopen('D:\test.imp','w');
-fprintf(fid,txt)
-fclose(fid);
